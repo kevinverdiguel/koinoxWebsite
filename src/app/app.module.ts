bcc614abff5c9b75c8c14b45ae8d4001c3ab9e80
Dell <Dell@DESKTOP-NOUQ59H>
2023-10-09 12:11:03 -0600

    Buttons and navbar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { InfoComponent } from './home/info/info.component';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,10 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot([
+      {path: 'info', component: InfoComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
