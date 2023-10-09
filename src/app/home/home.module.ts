4c94e3fd60e7b9a321f00659121272f385738fa2
Kevin Verdiguel <kevinvdflrs@gmail.com>
2023-10-08 22:46:46 -0600

    Finished components

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,21 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {GoogleMapsModule } from '@angular/google-maps'
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HeadComponent } from './head/head.component';
 import { InfoComponent } from './info/info.component';
 import { AboutComponent } from './about/about.component';
+import { ImagesComponent } from './images/images.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
   declarations: [
     HeadComponent,
     InfoComponent,
-    AboutComponent
+    AboutComponent,
+    ImagesComponent,
+    ContactComponent
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    GoogleMapsModule
   ]
 })
 export class HomeModule { }
